5b9e4d19abd9aace1ea4cabad90518a319ba82c1
admin <admin@iMac-de-admin-8.local>
2020-01-27 22:11:40 +0100

    level0 source

--- level0/source.c
+++ level0/source.c
@@ -8,16 +8,16 @@ int main(int ac, char **av, char **envp)
 {
 	char *sh;
 	gid_t gid;
-    uid_t uid;
+	uid_t uid;
 
 	if (atoi(av[1]) == 423)
 	{
 		sh = strdup("/bin/sh");
-    	gid = getegid();
-    	uid = geteuid();
+    		gid = getegid();
+    		uid = geteuid();
  
-    	setresgid(gid, gid, gid);
-    	setresuid(uid, uid, uid);
+    		setresgid(gid, gid, gid);
+    		setresuid(uid, uid, uid);
 		execve(sh, NULL, envp);
 	}
 	else
