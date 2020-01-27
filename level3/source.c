540d5e7a549186e68aad9fcdf9649a0d4e912d60
admin <admin@iMac-de-admin-8.local>
2020-01-27 22:18:00 +0100

    source level3 incomplete

--- level3/source.c
+++ level3/source.c
@@ -1,10 +1,13 @@
+// global i?
+int i = 0
+
 void v()
 {
 	char *str;
 
 	str = fgets(str, 512, stdin);
 	printf(str);
-	if (str == "64")
+	if (i == 64)
 	{
 		fwrite(1, "Wait what?!", 12);
 		system("/bin/sh");
