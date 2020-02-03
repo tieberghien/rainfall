113d4d10bff56a585dfa2d21704da18722cae4b0
Estelle TIEBERGHIEN <etieberg@e1r5p20.42.fr>
2020-02-03 16:50:26 +0100

    source bonus0

--- bonus0/source.c
+++ bonus0/source.c
@@ -1,22 +1,47 @@
-char *p(char *dash)
+char *p(buff, str)
 {
-	char buf[4096];
+	char *tmp_buff;
 
-	puts(dash);
-	read(0, buf, 4096);
-	strchr(buf, '\n');
-	char *ret = strncpy(ret, buf, 4096);
-	return (ret);
+	puts(str);
+	read(0,*tmp_buff, 4096);
+	strchr(*tmp_buff, '\n');
+	strncpy(buff,*tmp_buff, 20);
+	return ;
 }
 
-char *pp()
+char *pp(dest)
 {
-	char *dash = " - ";
+	char *str = " - ";
+	char *buff1; //0xbffff688
+	char *buff2; //0xbffff69c  // $ebp-0x1c
+	char *tmp;
+	unsigned int *n; //$ebp-0x3c
+	char carac;
 
-	char *str = p(dash);
+	p(buff1, str);
+	p(buff2, str);
+	strcpy(dest, buff1);
+	tmp = dest;
+	n = -1; //0xffffffff
+	carac = *tmp;
+	while(carac != '\0')        // size of buff1 before '\0'
+	{
+		if (n == 0)
+			break;
+		n--;
+		tmp++;
+		carac = tmp;
+	}
+	not n - 1;
+	dest + n = " ";
+	strcat(dest, buff2);
 }
 
 int main()
 {
-	puts(pp());
+	char dest[54]; //0x40 (64)
+
+	pp(dest);
+	puts(dest);
+	return(0);
 }
