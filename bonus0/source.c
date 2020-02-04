2d9a122c5297980d41c79459b3d57223e6c21a58
Estelle TIEBERGHIEN <etieberg@e1r8p9.42.fr>
2020-02-04 12:14:47 +0100

    jcapte r

--- bonus0/source.c
+++ bonus0/source.c
@@ -1,11 +1,11 @@
-char *p(buff, str)
+char *p(char *buff, char *str)
 {
-	char *tmp_buff;
+	char tmp[4096];
 
 	puts(str);
-	read(0,*tmp_buff, 4096);
-	strchr(*tmp_buff, '\n');
-	strncpy(buff,*tmp_buff, 20);
+	read(0, tmp, 4096);
+	strchr(tmp, '\n');
+	strncpy(buff, tmp, 20);
 	return ;
 }
 
@@ -14,26 +14,11 @@ char *pp(dest)
 	char *str = " - ";
 	char *buff1; //0xbffff688
 	char *buff2; //0xbffff69c  // $ebp-0x1c
-	char *tmp;
-	unsigned int *n; //$ebp-0x3c
-	char carac;
 
 	p(buff1, str);
 	p(buff2, str);
 	strcpy(dest, buff1);
-	tmp = dest;
-	n = -1; //0xffffffff
-	carac = *tmp;
-	while(carac != '\0')        // size of buff1 before '\0'
-	{
-		if (n == 0)
-			break;
-		n--;
-		tmp++;
-		carac = tmp;
-	}
-	not n - 1;
-	dest + n = " ";
+	dest[strlen(dest)] = " ";
 	strcat(dest, buff2);
 }
 
@@ -41,7 +26,6 @@ int main()
 {
 	char dest[54]; //0x40 (64)
 
-	pp(dest);
-	puts(dest);
+	puts(&dest);
 	return(0);
 }
