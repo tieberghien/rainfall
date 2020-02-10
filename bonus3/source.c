9bedd87de5e1b5291307939a3efbea48fa6351b2
admin <admin@iMac-de-admin-8.local>
2020-02-10 23:12:56 +0100

    all flags've been found. now onto to walkthroughs

--- bonus3/source.c
+++ bonus3/source.c
@@ -1,8 +1,22 @@
 int main(int ac, char **av)
 {
-	char str[160];
-	FILE *f;
+	FILE	*f;
+	int	buf[2600];
+	int	x;
+	char	buffer[66];
 
-	if ((f = fopen("/home/user/end/.pass", "r")) == -1)
-		return ;
+	f = fopen("/home/user/end/.pass", "r");
+	bzero(buf, 0x21 * 4);
+	if (f == NULL || ac != 2)
+		return (-1);
+	fread(buf, 1, 0x42, f);
+	x = atoi(av[1]);
+	buf[x] = 0;
+	fread(buffer, 1, 0x41, f);
+	fclose(f);
+	if (strcmp((char*)buf, av[1]) == 0)
+		execl("/bin/sh", "sh", 0);
+	else
+		puts(buffer);
+	return (0);
 }
